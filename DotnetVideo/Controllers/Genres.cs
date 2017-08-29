2da7daaa11ffe0bd262ab07e86dec64d82c8354a
Brandyn Sullins <woofenwauzer@gmail.com>
2017-08-29 11:09:46 -0400

    Controllers Made

---
+++ DotnetVideo/Controllers/Genres.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DotnetVideo.Models;
+
+namespace DotnetVideo.Controllers
+{
+    public class Genres : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Delete()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
